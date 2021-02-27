9057175f40aba925c8b537bdaf998a96eea9ce70
nat-nic <79392127+nat-nic@users.noreply.github.com>
2021-02-27 08:12:07 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,54 @@
+const a = 5
+const b = 4
+const result = a > b
+console.log(result)
+
+const c = "ананас"
+const d = "яблоко"
+const result1 = c > d
+console.log(result1)
+
+const e = '2'
+const f = '12'
+const result2 = e > f
+console.log(result2)
+
+const g = undefined;
+const h = null;
+const result3 = g == h;
+console.log(result3)
+
+const l = undefined;
+const m = null;
+const result4 = l === m;
+console.log(result4)
+
+let n = null
+let s = "\n0\n"
+let result5 = n == s
+console.log(result5)
+
+n = null;
+s = +"\n0\n";
+result5 = n === s;
+console.log({result5});
+
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true+false);
+console.log(6/"3");
+console.log("2"*"3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log(7 / 0);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n " - 2);
+
+
+
+
